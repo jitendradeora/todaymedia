bc18b0043d25edec0dcff7e0d8776f18d6981bff
jitendradeora <deorajitendra@gmail.com>
2025-10-30 00:03:16 +0400

    mobile menu header icons size changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -280,14 +280,14 @@ export default function Header() {
               className="md:hidden"
               onClick={() => setMobileSearchOpen(true)}
             >
-              <Search className="w-5 h-5" />
+              <Search className="w-6 h-6" />
             </Button>
 
             {/* Mobile Menu */}
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
               <SheetTrigger asChild>
                 <Button variant="ghost" size="icon">
-                  <Menu className="w-5 h-5" />
+                  <Menu className="w-6 h-6" />
                 </Button>
               </SheetTrigger>
               <SheetContent
